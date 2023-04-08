b4efe1be2c8e4cd70aa88677e54cbbd6c4e95fe6
Gaurav Shukla <heygauravshukla@gmail.com>
2023-04-08 09:36:15 +0530

    Done with mobile first FeaturedHero section

--- components/FeaturedHero.js
+++ components/FeaturedHero.js
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 const FeaturedHero = () => {
     return (
-        <section className="wrapper">
+        <section className="wrapper py-12 grid gap-10">
             <picture>
                 <source media="(min-width: 650px)" alt="Hero Image" srcSet="/desktop/image-interactive.jpg" />
                 <Image priority className="w-full" src="/mobile/image-interactive.jpg" alt="Hero Image" width={654} height={448} />
             </picture>
-            <div>
-                <h2>The leader in interactive VR</h2>
-                <p>Founded in 2011, Loopstudios has been producing world-class virtual reality
+            <div className="grid gap-4">
+                <h2 className="text-3xl uppercase font-secondary font-light text-center">The leader in interactive VR</h2>
+                <p className="text-center font-primary text-gray-500">Founded in 2011, Loopstudios has been producing world-class virtual reality
                     projects for some of the best companies around the globe. Our award-winning
                     creations have transformed businesses through digital experiences that bind
                     to their brand.</p>
--- components/Header.js
+++ components/Header.js
@@ -11,11 +11,11 @@ const Header = () => {
     }
 
     return (
-        <header className={`${isNavExpanded ? "fixed inset-x-4" : "static"} flex justify-between items-center z-30`}>
+        <header className={`${isNavExpanded ? "fixed inset-x-5" : "static"} flex justify-between items-center z-30`}>
             <Link href="/" className='z-30'>
                 <Image className='max-h-[1.5rem] w-auto' src="/logo.svg" alt="Logo Image" height={32} width={192} />
             </Link>
-            <button onClick={toggleMenu} className={`${isNavExpanded ? "bg-[url('/icon-close.svg')]" : "bg-[url('/icon-hamburger.svg')]"} w-[1rem] h-[1rem] bg-no-repeat bg-contain bg-center z-30`}></button>
+            <button onClick={toggleMenu} className={`${isNavExpanded ? "bg-[url('/icon-close.svg')]" : "bg-[url('/icon-hamburger.svg')]"} w-[1.5rem] h-[1.5rem] bg-no-repeat bg-contain bg-center z-30`}></button>
             <ul className={`${isNavExpanded ? "translate-x-0" : "translate-x-full"} fixed inset-0 px-4 bg-black min-h-screen uppercase font-light flex flex-col justify-center gap-2 text-2xl text-gray-300 transition-all z-20`}>
                 <li><Link href="/">About</Link></li>
                 <li><Link href="/">Careers</Link></li>
